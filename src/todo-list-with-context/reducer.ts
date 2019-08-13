0925c1d7278d48399593f8bb7d73b0620d62b675
tianyanqiu <tianyanqiu@sinosoft.com.cn>
2019-08-13 14:14:22 +0800

    chore:调整reducer的处理逻辑

--- src/todo-list-with-context/reducer.ts
+++ src/todo-list-with-context/reducer.ts
@@ -7,31 +7,28 @@ const todoReducer = produce((draft: Todo[], action: Action) => {
       draft.push(action.payload);
       break;
     case "TOGGLE_TODO":
-      return draft.map(todo =>
-        todo.id === action.payload
-          ? produce(todo, sta => {
-              sta.completed = !sta.completed;
-            })
-          : todo
-      );
+      const selectedItem = draft.find(todo => todo.id === action.payload);
+      if (selectedItem) {
+        selectedItem.completed = !selectedItem.completed;
+      }
+      break;
     case "DELETE_TODO":
-      return draft.filter(todo => todo.id !== action.payload);
-
+      draft.splice(draft.findIndex(todo => todo.id === action.payload), 1);
+      break;
     case "DELETE_ALL_COMPLETED_TODOS":
       return draft.filter(todo => !todo.completed);
-
     default:
       return draft;
   }
 });
 
-const visibilityFilterReducer = produce((draft: string, action: Action) => {
+const visibilityFilterReducer = (state: string, action: Action) => {
   switch (action.type) {
     case "SET_VISIBILITY_FILTER":
       return action.payload;
     default:
-      return draft;
+      return state;
   }
-});
+};
 
 export { todoReducer, visibilityFilterReducer };
